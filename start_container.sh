85e3afa943ea07413cb32f51273c6022b2fae4bb
Prateek097 <prateekpal99810@gmail.com>
2025-01-11 00:04:51 +0530

    add the files for code deploy

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull prateek097/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p  5000:5000 prateek097/simple-python-flask-app
